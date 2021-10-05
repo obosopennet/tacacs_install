8e22576ae47878bd1419351862e7a1fb47af87fe
Tom Sverre <68464234+HumlePung@users.noreply.github.com>
2021-10-05 14:58:25 +0200

    Update install-tacacs.sh

--- install-tacacs.sh
+++ install-tacacs.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 
+devfile=DEVEL.202109260929.tar.bz2
+
+echo "#######################################"
 echo ' Will install Tacacs and it dependecies'
+echo "#######################################"
 
 wait 3
 
+echo "##############################################################################"
 echo 'Install dependecies, gcc make libpcre3-dev libbind-dev libpam0g-dev libssl-dev'
+echo "##############################################################################"
+
 
 apt-get install gcc make libpcre3-dev libbind-dev libpam0g-dev libssl-dev
 
+echo "#############################################################################"
 echo 'Download Tacacs'
+echo '#############################################################################'
 
-wget https://www.pro-bono-publico.de/projects/src/DEVEL.202109260929.tar.bz2
+wget https://www.pro-bono-publico.de/projects/src/$devfile
+
+
+echo "#############################################################################"
+echo 'Untar' $devfile
+echo '#############################################################################'
+
+tar xjvf $devfile
