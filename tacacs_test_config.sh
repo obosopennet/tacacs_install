5a2a01a0483c3fa5df4016baf31dcde4759742f1
Tom Sverre <68464234+HumlePung@users.noreply.github.com>
2021-10-06 20:50:51 +0200

    Update tacacs_test_config.sh

--- tacacs_test_config.sh
+++ tacacs_test_config.sh
@@ -1,1 +1,11 @@
+ #!/bin/sh
+ 
+ echo "##################################"
+ echo " Testing the tac_plus.cfg file after changes.
+        If no Output then Ok, if some output fix the issue.
+        "
+ sleep 10
+ 
  /usr/local/sbin/tac_plus -P /usr/local/etc/tac_plus.cfg
+
+echo "########## Done #########"
